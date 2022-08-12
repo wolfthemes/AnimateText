69b232aa82ba6df209d3bda82e770e5eeb00753b
WolfThemes <contact@wolfthemes.com>
2022-08-12 15:58:16 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# AnimateText.js
+
+## A super minimal JS script to animate text using SplitType
+
+It can be used to animate text on page load, or when the text appears on screen in combination with wow.js or similar.
+
---
+++ src/js/index.js
@@ -0,0 +1,50 @@
+class AnimateText {
+
+    element;
+    type;
+    
+    constructor( element, type ) {
+
+        const { animationDelay } = element.dataset;
+
+        this.element = element;
+        this.type = type;
+
+        this.delay = Number(animationDelay);
+        
+        this.init();
+
+        // Re-initialize the Split Text on window resize.
+        window.addEventListener("resize", () => {
+            this.init();
+        });
+    }
+
+    init() {
+        
+        // Create SplitType
+        new SplitType( this.element, { types: this.type + 's' } );
+
+        var currentType = this.type,
+            wrapline = this.wrapLine,
+            delay = this.delay / 1000;
+
+        // Add delays
+        this.element.querySelectorAll("." + this.type).forEach(function(el, index){
+            el.style.transitionDelay = `${
+            delay ? delay + index / 50 : index / 50
+            }s`;
+
+            if ( "line" === currentType ) {
+                wrapline( el );
+            }
+        } );
+    }
+
+    wrapLine(el, wrapper) {
+        wrapper = wrapper || document.createElement( "div" );
+        wrapper.classList = "line-wrapper";
+        el.parentNode.insertBefore(wrapper, el);
+        wrapper.appendChild(el);
+    }
+}
